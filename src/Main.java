c5cdce3ed0dbe7b6213376065c1d46a4c137c83b
LenaGritse <mironuk.ea@gmail.com>
2020-06-14 08:29:15 +0300

    HomeWork1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
+    Competitor[] competitors = {
+      new Human("Ivan", 3000, 2),
+      new Robot("RoboSport", 10000, 5),
+      new Cat("Bars", 300, 1)
+    };
+
+    Obstacle[] obstacles = {
+       new Treadmill(1000),
+       new Wall(3)
+    };
+
+    for (Competitor c : competitors) {
+        for (Obstacle o : obstacles) {
+            o.doIt(c);
+            if (!c.isOnDistance()) {
+                break;
+            }
+        }
+    }
+
+    for (Competitor competitor : competitors) {
+        competitor.info();
+    }
+
     }
 }
