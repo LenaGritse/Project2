a1442163c651b56f6e35e967e9cbc5d72f086c6f
LenaGritse <mironuk.ea@gmail.com>
2020-06-30 00:54:09 +0300

    HomeWork5/2

--- src/lesson5/RunnableMillis.java
+++ src/lesson5/RunnableMillis.java
@@ -10,7 +10,7 @@ public class RunnableMillis implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + 5000000 / 5) * Math.cos(0.2f + 5000000 / 5) * Math.cos(0.4f + 5000000 / 2));
         }
     }
 }
